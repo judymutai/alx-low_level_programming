7cd8c239cfce8910303748481a65a600671ca82c
judymutai <judymutai4@gmail.com>
2022-07-01 19:47:51 +0300

    c task 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,31 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdlo.h>
 
 /**
- * main -> assign a random number to the variable n each time it is executed and prints out
- * based a condition 
- * Return : always 0
- */
+ * *main - Start here
+ * *Random generated numbers and check if positive, zero or negative.
+ * *Return: value 0 (Accept)
+ * */
+
 int main(void)
 {
 		int n;
 
 			srand(time(0));
 				n = rand() - RAND_MAX / 2;
-				if (n > 00
-						printf("%d is positive", n)
-					return (0);
+					if (n < 0)
+							{
+										printf("%d is negative\n", n);
+											}
+						else if (n > 0)
+								{
+											printf("%d is positive\n", n);
+												}
+							else
+									{
+												printf("%d is zero\n", n);
+													}
+
+								return (0);
 }
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * *main - Begin Here
+ * *print a-z and A-Z using putchar
+ * *Return: Value 0 (true)
+ * */
+int main(void)
+{
+		char ch = 'a';
+			char CH = 'A';
+
+				while (ch <= 'z')
+						{
+									putchar(ch);
+											ch++;
+												}
+					while (CH <= 'Z')
+							{
+										putchar(CH);
+												CH++;
+													}
+						putchar('\n');
+							return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
